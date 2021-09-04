342937f545e9fbd09a38c8152b103ba64dd6e0a0
Bulat Maskurov <mbmhd2015@gmail.com>
2021-09-04 14:55:26 +0300

    created AbstractPage class

--- src/test/java/ru/bulkashmak/pages/AbstractPage.java
+++ src/test/java/ru/bulkashmak/pages/AbstractPage.java
@@ -1,2 +1,39 @@
-package ru.bulkashmak.pages;public class AbstractPage {
+package ru.bulkashmak.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import static org.junit.jupiter.api.Assertions.*;
+import ru.bulkashmak.util.WebDriverWrapper;
+
+public abstract class AbstractPage {
+
+    public static Logger log = LogManager.getRootLogger();
+
+    public WebDriverWrapper driver = WebDriverWrapper.getInstance();
+    public String baseUrl;
+
+    public AbstractPage(String baseUrl) {
+
+        this.baseUrl = baseUrl;
+    }
+
+    public void open(){
+
+        driver.get(baseUrl);
+        log.info("Страница по адресу '{}' открыта.", baseUrl);
+    }
+
+    public void close(){
+
+        driver.close();
+        log.info("Страница по адресу '{}' была закрыта.", baseUrl);
+    }
+
+    public void checkPage(){
+
+        log.info("Осуществляю проверку что мы находимся на странице по адресу '{}'.", baseUrl);
+        assertTrue(
+                driver.getCurrentUrl().contains(baseUrl)
+        );
+    }
 }
