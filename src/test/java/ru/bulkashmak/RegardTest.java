afdde6517d5afd65cd576c1018cc39db63add027
Bulat Maskurov <mbmhd2015@gmail.com>
2021-09-04 14:55:35 +0300

    refactor

--- src/test/java/ru/bulkashmak/RegardTest.java
+++ src/test/java/ru/bulkashmak/RegardTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AppTest {
+public class RegardTest {
     @Test
     void test() {
         WebDriver driver = new ChromeDriver();
